8c90ff2e2013d4ef5f1d9e56fe8254e1306689c7
gbnnbg <140027816+gbnnbg@users.noreply.github.com>
2024-12-20 16:39:40 +0300

    Update topic.sh

--- youbot/src/topic.sh
+++ youbot/src/topic.sh
@@ -1,10 +1,10 @@
 ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 5 }" -t 10
-ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 1
-ros2 topic pub /cmd_vel geometry_msgs/Twist '{linear: {x: 0, y: 0, z: 0}, angular: {x: 0, y: 10, z: 0}}' -t 2
-ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 1
+ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 2
+ros2 topic pub /cmd_vel geometry_msgs/Twist '{linear: {x: 0, y: 0, z: 0}, angular: {x: 0, y: 14, z: 0}}' -t 1
+ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 2
 ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 10 }" -t 7
-ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 1
+ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 2
 ros2 topic pub /cmd_vel geometry_msgs/Twist '{linear: {x: 0, y: 0, z: 0}, angular: {x: 0, y: -8, z: 0}}' -t 3
-ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 1
+ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 2
 ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 3 }" -t 5
 ros2 topic pub /cmd_vel geometry_msgs/Twist  "linear: { x: 0 }" -t 1
